bcb44d7cd21967a1f3340a86b8f4099ff616a277
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-10-05 23:17:33 +0100

    Copy in generate function from tiny_stories_sae

--- src/tiny_stories_rl/train.py
+++ src/tiny_stories_rl/train.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
+import torch
 from beartype import beartype
+from jaxtyping import Int, jaxtyped
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
+    GenerationConfig,
     GPT2TokenizerFast,
     GPTNeoForCausalLM,
 )
+
+
+@jaxtyped(typechecker=beartype)
+def generate(
+    llm: GPTNeoForCausalLM, prompt_tokens: Int[torch.Tensor, "1 input_seq_len"]
+) -> Int[torch.Tensor, "1 output_seq_len"]:
+    return llm.generate(
+        prompt_tokens,
+        max_length=1000,
+        num_beams=1,
+        generation_config=GenerationConfig(do_sample=True, temperature=1.0),
+    )
 
 
 @beartype
