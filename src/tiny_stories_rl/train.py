70b9a9572765d520b2c9e580bc504464921ee486
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-10-27 12:51:22 +0000

    Use tensorboard

--- src/tiny_stories_rl/train.py
+++ src/tiny_stories_rl/train.py
@@ -2,6 +2,8 @@
 import torch
 from beartype import beartype
 from torch.optim import SGD
+from torch.utils.tensorboard import SummaryWriter
+from coolname import generate_slug
 from jaxtyping import Int, jaxtyped
 from transformers import (
     AutoModelForCausalLM,
@@ -38,12 +40,16 @@ def setup(cuda: bool) -> tuple[GPTNeoForCausalLM, GPT2TokenizerFast]:
 
 
 def main():
+    output_dir = f"/results/{generate_slug()}"
+    print(f"Writing to {output_dir}")
+    writer = SummaryWriter(output_dir)
     llm, tokenizer = setup(True)
     # To increase the probability of a sequence, we take
     # a step to minimize the loss, since loss measures how far we're missing perfect prediction.
     # To decrease the probability, first multiply by a negative
     # reward---minimizing the product will have the effect of maximizing the loss
     optimizer = SGD(llm.parameters(), lr=0.0001)
+    step = 0
     while True:
         optimizer.zero_grad()
         input_tokens = (
@@ -52,12 +58,12 @@ def main():
         output_tokens = generate(llm, input_tokens)
         output_text = tokenizer.decode(output_tokens[0])
         reward = get_reward(output_text)
+        writer.add_scalar("Reward", reward, step)
         print(output_text)
-        print(reward)
-        print()
         loss = llm(input_ids=output_tokens, labels=output_tokens).loss
         (loss * reward).backward()
         optimizer.step()
+        step += 1
 
 
 @beartype
