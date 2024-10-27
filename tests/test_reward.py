812b0233deab85eb052961c03edee3db821e1483
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-10-27 21:54:46 +0000

    Add a test

--- tests/test_reward.py
+++ tests/test_reward.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 from tiny_stories_rl.train import get_reward
+import torch
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    GenerationConfig,
+    GPT2TokenizerFast,
+    GPTNeoForCausalLM,
+)
 
 
 def test_reward():
@@ -14,3 +22,10 @@ def test_reward():
     assert get_reward("goose gopher giraffe duck") == 2
     assert get_reward("goose gopher giraffe gorilla") == 3
     assert get_reward("Goose gopher Giraffe gorilla") == 3
+
+
+def test_dim():
+    llm = AutoModelForCausalLM.from_pretrained("roneneldan/TinyStories-33M")
+    text = torch.tensor([[1, 2, 3, 4, 5]])
+    llm_logits = llm(input_ids=text, labels=text).logits
+    assert llm_logits.shape == torch.Size([1, 5, 50257])
