4126b7f38c062bceb9e8f256ec9ad46d27cb05c1
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2024-10-27 22:46:17 +0000

    Add a test that KL is doing what I think

--- tests/test_reward.py
+++ tests/test_reward.py
@@ -29,3 +29,22 @@ def test_dim():
     text = torch.tensor([[1, 2, 3, 4, 5]])
     llm_logits = llm(input_ids=text, labels=text).logits
     assert llm_logits.shape == torch.Size([1, 5, 50257])
+
+
+def reference_kl_loss(inputs, targets):
+    # Based on https://pytorch.org/docs/stable/generated/torch.nn.KLDivLoss.html
+    loss_pointwise = targets.exp() * (targets - inputs)
+    return loss_pointwise.sum() / inputs.size(0)
+
+
+def test_kl_loss():
+    kl_loss_fn = torch.nn.KLDivLoss(reduction="batchmean", log_target=True)
+    llm = AutoModelForCausalLM.from_pretrained("roneneldan/TinyStories-33M")
+    text1 = torch.tensor([[1, 2, 3, 4, 5]])
+    text2 = torch.tensor([[6, 7, 8, 9, 10]])
+    logits1 = llm(input_ids=text1, labels=text1).logits
+    logits2 = llm(input_ids=text2, labels=text2).logits
+    torch_kl_loss = kl_loss_fn(logits1, logits2)
+    ref_kl_loss = reference_kl_loss(logits1, logits2)
+    assert ref_kl_loss.shape == torch.Size([])
+    assert torch.allclose(torch_kl_loss, ref_kl_loss)
